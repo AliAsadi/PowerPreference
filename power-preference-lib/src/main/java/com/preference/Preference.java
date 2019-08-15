729fd7ac7de69e543156951acf7c8ae3918b17c7
Ali Asadi <aliesaassadi@gmail.com>
2019-08-15 19:58:20 +0300

    update preference class documentation

--- power-preference-lib/src/main/java/com/preference/Preference.java
+++ power-preference-lib/src/main/java/com/preference/Preference.java
@@ -16,9 +16,11 @@ public interface Preference {
     /**
      * Store int in SharedPreference Data.
      *
+     * Writes the data asynchronously
+     *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
-     * @return a reference to the same {@link Preference} object, so you can
+     * @return A reference to the same {@link Preference} object, so you can
      * chain put calls together.
      */
     Preference putInt(String key, int value);
@@ -26,9 +28,11 @@ public interface Preference {
     /**
      * Store long in SharedPreference Data.
      *
+     * Writes the data asynchronously
+     *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
-     * @return a reference to the same {@link Preference} object, so you can
+     * @return A reference to the same {@link Preference} object, so you can
      * chain put calls together.
      */
     Preference putLong(String key, long value);
@@ -36,9 +40,11 @@ public interface Preference {
     /**
      * Store float in SharedPreference Data.
      *
+     * Writes the data asynchronously
+     *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
-     * @return a reference to the same {@link Preference} object, so you can
+     * @return A reference to the same {@link Preference} object, so you can
      * chain put calls together.
      */
     Preference putFloat(String key, float value);
@@ -46,9 +52,11 @@ public interface Preference {
     /**
      * Store double in SharedPreference Data.
      *
+     * Writes the data asynchronously
+     *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
-     * @return a reference to the same {@link Preference} object, so you can
+     * @return A reference to the same {@link Preference} object, so you can
      * chain put calls together.
      */
     Preference putDouble(String key, double value);
@@ -56,9 +64,11 @@ public interface Preference {
     /**
      * Store boolean in SharedPreference Data.
      *
+     * Writes the data asynchronously
+     *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
-     * @return a reference to the same {@link Preference} object, so you can
+     * @return A reference to the same {@link Preference} object, so you can
      * chain put calls together.
      */
     Preference putBoolean(String key, boolean value);
@@ -66,9 +76,11 @@ public interface Preference {
     /**
      * Store String in SharedPreference Data.
      *
+     * Writes the data asynchronously
+     *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
-     * @return a reference to the same {@link Preference} object, so you can
+     * @return A reference to the same {@link Preference} object, so you can
      * chain put calls together.
      */
     Preference putString(String key, String value);
@@ -76,9 +88,11 @@ public interface Preference {
     /**
      * Store Object in SharedPreference Data.
      *
+     * Writes the data asynchronously
+     *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
-     * @return a reference to the same {@link Preference} object, so you can
+     * @return A reference to the same {@link Preference} object, so you can
      * chain put calls together.
      */
     Preference putObject(String key, Object value);
@@ -87,85 +101,85 @@ public interface Preference {
 
     /**
      * Store int in SharedPreference Data.
-     * <p><p>
-     * commit() writes the data synchronously (blocking the thread its called from).
+     * <p>
+     * Writes the data synchronously (blocking the thread its called from).
      * It then informs you about the success of the operation
      *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
-     * @return returns true if the save works, false otherwise.
+     * @return True if the save works, false otherwise.
      */
     boolean setInt(String key, int value);
 
     /**
      * Store long in SharedPreference Data.
-     * <p><p>
-     * commit() writes the data synchronously (blocking the thread its called from).
+     * <p>
+     * Writes the data synchronously (blocking the thread its called from).
      * It then informs you about the success of the operation
      *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
-     * @return returns true if the save works, false otherwise.
+     * @return True if the save works, false otherwise.
      */
     boolean setLong(String key, long value);
 
     /**
      * Store float in SharedPreference Data.
-     * <p><p>
-     * commit() writes the data synchronously (blocking the thread its called from).
+     * <p>
+     * Writes the data synchronously (blocking the thread its called from).
      * It then informs you about the success of the operation
      *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
-     * @return returns true if the save works, false otherwise.
+     * @return True if the save works, false otherwise.
      */
     boolean setFloat(String key, float value);
 
     /**
      * Store double in SharedPreference Data.
-     * <p><p>
-     * commit() writes the data synchronously (blocking the thread its called from).
+     * <p>
+     * Writes the data synchronously (blocking the thread its called from).
      * It then informs you about the success of the operation
      *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
-     * @return returns true if the save works, false otherwise.
+     * @return True if the save works, false otherwise.
      */
     boolean setDouble(String key, double value);
 
     /**
      * Store boolean in SharedPreference Data.
-     * <p><p>
-     * commit() writes the data synchronously (blocking the thread its called from).
+     * <p>
+     * Writes the data synchronously (blocking the thread its called from).
      * It then informs you about the success of the operation
      *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
-     * @return returns true if the save works, false otherwise.
+     * @return True if the save works, false otherwise.
      */
     boolean setBoolean(String key, boolean value);
 
     /**
      * Store String in SharedPreference Data.
-     * <p><p>
-     * commit() writes the data synchronously (blocking the thread its called from).
+     * <p>
+     * Writes the data synchronously (blocking the thread its called from).
      * It then informs you about the success of the operation
      *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
-     * @return returns true if the save works, false otherwise.
+     * @return True if the save works, false otherwise.
      */
     boolean setString(String key, String value);
 
     /**
      * Store Object in SharedPreference Data.
-     * <p><p>
-     * commit() writes the data synchronously (blocking the thread its called from).
+     *
+     * Writes the data synchronously (blocking the thread its called from).
      * It then informs you about the success of the operation
      *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
-     * @return returns true if the save works, false otherwise.
+     * @return True if the save works, false otherwise.
      */
     boolean setObject(String key, Object value);
 
@@ -175,77 +189,104 @@ public interface Preference {
      * Retrieve a String value from the preferences
      *
      * @param key - The name of the preference to retrieve.
-     * @return Returns the preference value if it exists, or defValue.
-     * if preference value did'nt exists and defValue didn't exists will return {@value ""} empty string.
+     * @return Preference value if it exists otherwise, returns "".
      */
     String getString(String key);
 
+    /**
+     * Retrieve a String value from the preferences
+     *
+     * @param key      - The name of the preference to retrieve.
+     * @param defValue - Value to return if this preference does not exist.
+     * @return Preference value if it exists otherwise, returns defValue.
+     */
     String getString(String key, String defValue);
 
     /**
      * Retrieve a int value from the preferences
      *
      * @param key - The name of the preference to retrieve.
-     * @return Returns the preference value if it exists, or defValue.
-     * if preference value did'nt exists and defValue didn't exists will return {@linkplain 0}.
-     * @throws WrongValueException print error exception to the console if used
-     *                             an invalid value type for the default key.
+     * @return Preference value if it exists otherwise, returns 0.
      */
     int getInt(String key);
 
+    /**
+     * Retrieve a int value from the preferences
+     *
+     * @param key      - The name of the preference to retrieve.
+     * @param defValue - Value to return if this preference does not exist.
+     * @return Preference value if it exists otherwise, returns defValue.
+     */
     int getInt(String key, int defValue);
 
     /**
      * Retrieve a long value from the preferences
      *
      * @param key - The name of the preference to retrieve.
-     * @return Returns the preference value if it exists, or defValue.
-     * if preference value did'nt exists and defValue didn't exists will return {@linkplain 0}.
-     * @throws WrongValueException print error exception to the console if used
-     *                             an invalid value type for the default key.
+     * @return Preference value if it exists otherwise, returns 0.
      */
     long getLong(String key);
 
+    /**
+     * Retrieve a long value from the preferences
+     *
+     * @param key      - The name of the preference to retrieve.
+     * @param defValue - Value to return if this preference does not exist.
+     * @return RPreference value if it exists otherwise, returns defValue.
+     */
     long getLong(String key, long defValue);
 
     /**
      * Retrieve a boolean value from the preferences
      *
      * @param key - The name of the preference to retrieve.
-     * @return Returns the preference value if it exists, or defValue.
-     * if preference value did'nt exists and defValue didn't exists will return {@linkplain false}.
-     * @throws WrongValueException print error exception to the console if used
-     *                             an invalid value type for the default key.
+     * @return Preference value if it exists otherwise, returns false.
      */
     boolean getBoolean(String key);
 
+    /**
+     * Retrieve a boolean value from the preferences
+     *
+     * @param key      - The name of the preference to retrieve.
+     * @param defValue - Value to return if this preference does not exist.
+     * @return Preference value if it exists otherwise, returns defValue.
+     */
     boolean getBoolean(String key, boolean defValue);
 
     /**
      * Retrieve a float value from the preferences
      *
      * @param key - The name of the preference to retrieve.
-     * @return Returns the preference value if it exists, or defValue.
-     * if preference value did'nt exists and defValue didn't exists will return {@linkplain 0}.
-     * @throws WrongValueException print error exception to the console if used
-     *                             an invalid value type for the default key.
+     * @return Preference value if it exists otherwise, returns 0.
      */
     float getFloat(String key);
 
+    /**
+     * Retrieve a float value from the preferences
+     *
+     * @param key      - The name of the preference to retrieve.
+     * @param defValue - Value to return if this preference does not exist.
+     * @return Preference value if it exists otherwise, returns defValue.
+     */
     float getFloat(String key, float defValue);
-
-    <T> T getObject(String key, Class classType, T defValue);
 
     /**
      * Retrieve a Object value from the preferences
      *
      * @param key - The name of the preference to retrieve.
-     * @return Returns the preference value if it exists, or defValue.
-     * <p>
-     * if preference value did'nt exists and defValue didn't exists will return {@linkplain null}.
+     * @return Preference value if it exists otherwise, returns null.
      */
     @SuppressWarnings("TypeParameterUnusedInFormals")
     <T> T getObject(String key, Class classType);
+
+    /**
+     * Retrieve a Object value from the preferences
+     *
+     * @param key      - The name of the preference to retrieve.
+     * @param defValue - Value to return if this preference does not exist.
+     * @return Preference value if it exists otherwise, returns defValue.
+     */
+    <T> T getObject(String key, Class classType, T defValue);
 
     /**
      * Retrieve a Map value from the preferences
@@ -254,7 +295,7 @@ public interface Preference {
      * @param classType - the type of the Map -> HashMap,TreeMap..etc.
      * @param keyType   - the type of the key that used in the map.
      * @param valueType - the type of the value that used in the map.
-     * @return Returns the preference value if it exists, or null.
+     * @return Preference value if it exists otherwise, returns null.
      */
     @SuppressWarnings("TypeParameterUnusedInFormals")
     <T> T getMap(String key, Class classType, Class keyType, Class valueType);
@@ -263,22 +304,24 @@ public interface Preference {
      * Retrieve a double value from the preferences
      *
      * @param key - The name of the preference to retrieve.
-     * @return Returns the preference value if it exists, or defValue.
-     * <p>
-     * if preference value did'nt exists and defValue didn't exists will return {@linkplain 0}.
-     * @throws WrongValueException print error exception to the console if used
-     *                             an invalid value type for the default key.
+     * @return Preference value if it exists otherwise, returns 0.
      */
     double getDouble(String key);
 
+    /**
+     * Retrieve a double value from the preferences
+     *
+     * @param key      - The name of the preference to retrieve.
+     * @param defValue - Value to return if this preference does not exist.
+     * @return Preference value if it exists otherwise, returns defValue.
+     */
     double getDouble(String key, double defValue);
 
     //////////////////// OTHER /////////////////////////
 
     /**
      * @param key - key name.
-     * @return Returns true if the preference exists in the preferences,
-     * otherwise false.
+     * @return True if preference value exists otherwise, false.
      */
     boolean contains(String key);
 
@@ -290,10 +333,13 @@ public interface Preference {
     void remove(String key);
 
     /**
-     * remove all data from this preference.
+     * Remove all data from this preference file.
      */
     void clear();
 
+    /**
+     * Retrieve all data of this preference file.
+     */
     Map<String, ?> getData();
 
     //////////////////// DEFAULTS /////////////////////////
